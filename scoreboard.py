76da9b4eb137d660b7c59511711589174ccec227
obed <aguochaobed1@gmail.com>
2024-07-08 14:41:36 +0100

    corrected mistake on scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -10,8 +10,6 @@ class Scoreboard(Turtle):
         self.score = 0
         with open("data.txt") as data:
             self.high_score = int(data.read())
-            with open("data.txt", mode="w") as data:
-                data.write(f"{self.high_score}")
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -25,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
